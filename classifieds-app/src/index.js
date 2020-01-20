a1d933506bf043e3fd9e4fdcf564d23873e05f5c
baconYao <bacon735392@gmail.com>
2020-01-21 01:45:55 +0800

    implemented the login functionality

--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -3,10 +3,12 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 import { render } from "react-dom";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 
 import graphqlClient from "#root/api/graphqlClient";
 import Root from "#root/components/Root";
 
+import store from "./store";
 import * as theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
@@ -25,11 +27,13 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 render(
-  <ApolloProvider client={graphqlClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Root />
-    </ThemeProvider>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={graphqlClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Root />
+      </ThemeProvider>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById("app")
 );
