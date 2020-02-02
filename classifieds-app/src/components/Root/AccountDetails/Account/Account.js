c6ad6c0e52455f968cc6db809ee9e504de942361
baconYao <bacon735392@gmail.com>
2020-02-02 22:49:33 +0800

    added deleteUser

--- classifieds-app/src/components/Root/AccountDetails/Account/Account.js
+++ classifieds-app/src/components/Root/AccountDetails/Account/Account.js
@@ -1,6 +1,8 @@
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import React from "react";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { clearSession } from "#root/store/ducks/session";
 import styled from "styled-components";
 
 const Email = styled.div`
@@ -9,17 +11,36 @@ const Email = styled.div`
   margin-top: .25rem;
 `;
 
+const LogoutLink = styled.a.attrs({ href: "#" })`
+  color: blue;
+  display: block;
+  margin-top: .25rem;
+`;
+
 const Wrapper = styled.div`
   color: ${props => props.theme.mortart};
   font-size: .9rem;
 `;
 
+const mutation = gql`
+  mutation($sessionId: ID!) {
+    deleteUserSession(sessionId: $sessionId)
+  }
+`;
+
 const Account = () => {
+  const [deleteUserSession] = useMutation(mutation);
+  const dispatch = useDispatch();
   const session = useSelector(state => state.session);
   
   return (
     <Wrapper>
       Logged in as <Email>{session.user.email}</Email>
+      <LogoutLink onClick={ event => {
+        event.preventDefault();
+        dispatch(clearSession());
+        deleteUserSession({ variables: {sessionId: session.id}})  
+      }}>(Logout)</LogoutLink>
     </Wrapper>
   );
 };
